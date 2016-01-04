05b004d7bb8378be6d28b957f492cc2c58695930
ZimonDai <daizhuoxian@gmail.com>
2016-01-03 19:35:40 -0800

    finish tut9

---
+++ mytut9/vertexShaderWithMatrix.glsl
@@ -0,0 +1,20 @@
+#version 330
+
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec4 color;
+out vec4 theColor;
+
+uniform float fLoopDuration;
+uniform float elapsedTime;
+uniform float offsetZ;
+uniform mat4 perspectiveMatrix;
+
+void main()
+{
+	theColor = color;
+    float fScale = 3.14159f * 2.0f / fLoopDuration;
+
+	float currentTime = mod(elapsedTime / 1000.0f, fLoopDuration);
+	vec4 offset = vec4(cos(currentTime * fScale) * 0.5f, sin(currentTime * fScale) * 0.5f, offsetZ, 0);
+	gl_Position = perspectiveMatrix * (offset + position);
+}
